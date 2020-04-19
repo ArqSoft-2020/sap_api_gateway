b9fd43a7bae0a2b93a6c9a3fc7bafe7f056c5223
Fernando Morantes <josefernandomorantes@gmail.com>
2020-04-19 16:20:53 -0500

    Add canvas api gateway

--- src/graphQLSchema.js
+++ src/graphQLSchema.js
@@ -10,7 +10,14 @@ import {
 	usersTypeDef
 } from './sap_profile_ms_api/profileTypeDef';
 
+import {
+	canvasMutations,
+	canvasQueries,
+	canvasTypeDef
+} from './sap_canvas_ms_api/canvasTypeDef';
+
 import usersResolvers from './sap_profile_ms_api/resolvers';
+import canvasResolvers from './sap_profile_ms_api/resolvers';
 
 // merge the typeDefs
 const mergedTypeDefs = mergeSchemas(
@@ -26,11 +33,35 @@ const mergedTypeDefs = mergeSchemas(
 	]
 );
 
+// merge the typeDefs
+const mergedTypeDefsCanvas = mergeSchemas(
+	[
+		'scalar JSON',
+		canvasTypeDef
+	],
+	[
+		canvasQueries
+	],
+	[
+		canvasMutations
+	]
+);
+
 // Generate the schema object from your types definition.
 export default makeExecutableSchema({
 	typeDefs: mergedTypeDefs,
 	resolvers: merge(
 		{ JSON: GraphQLJSON }, // allows scalar JSON
 		usersResolvers
+	)
+});
+
+// Generate the schema object from your types definition.
+export default makeExecutableSchema({
+	typeDefs: mergedTypeDefsCanvas,
+	resolvers: merge({
+			JSON: GraphQLJSON
+		}, // allows scalar JSON
+		canvasResolvers
 	)
 });
