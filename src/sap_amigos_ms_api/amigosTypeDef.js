6e6f574123dc14859740a20b9e6e6e51c8f92124
caricoe <caricoe@unal.edu.co>
2020-04-22 21:59:05 -0500

    model all

--- src/sap_amigos_ms_api/amigosTypeDef.js
+++ src/sap_amigos_ms_api/amigosTypeDef.js
@@ -1,30 +1,45 @@
 
 export const amigosTypeDef = `
-type ViewModelAmistad {
-    id: Int
-    Amigo1: Int
-    Amigo2: Int
-}
+
 input ViewModelAmistadInput {
-    Amigo1: Int
-    Amigo2: Int
+    amigo1: Int
+    amigo2: Int
 }
 
+
 type ViewModelResponseA {
-    error: Boolean
-    response: String
-    amistad: ViewModelAmistad
+    success: Boolean
+}
+
+type ViewModelResponseD {
+  deleted: String,
+  deleted2: String,
+  success: Boolean
+}
+
+type ViewModelResponseQ {
+  amigos1ra:[ViewModelResponseSubQ1]
+  amigos2da:[ViewModelResponseSubQ2]
+  success: Boolean
+}
+
+type ViewModelResponseSubQ1 {
+  amigo1: Int
+}
+
+type ViewModelResponseSubQ2 {
+  amigo2: Int
 }
 
 
 `;
 
 export const amigosQueries = `
-    AmigosInfo(id: Int!): ViewModelResponseA!
+    AmigosInfo(id: Int!): ViewModelResponseQ!
 `;
 
 export const amigosMutations = `
     NewAmistad(model: ViewModelAmistadInput!): ViewModelResponseA!
-    DeleteAmistad(Amigo1: Int!,Amigo2: Int!): ViewModelResponseA!
+    DeleteAmistad(amigo1: Int!,amigo2: Int!): ViewModelResponseD!
 
 `;
