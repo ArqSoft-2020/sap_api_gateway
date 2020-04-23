3e52d1d6774ee1ca47f49b1c287ff9b03aa2b5be
caricoe <caricoe@unal.edu.co>
2020-04-22 19:34:34 -0500

    cambio graphQLShema 2

--- src/graphQLSchema.js
+++ src/graphQLSchema.js
@@ -14,7 +14,7 @@ import {
 	amigosMutations,
 	amigosQueries,
 	amigosTypeDef
-} from './sap_amigos_ms_api/profileTypeDef';
+} from './sap_amigos_ms_api/amigosTypeDef';
 
 import amigosResolvers from './sap_amigos_ms_api/amigosresolvers';
 
